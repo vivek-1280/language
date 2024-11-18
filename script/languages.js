9b498ce10f2665b780d871a62878b42df2af7df7
vivek-1280 <newnewcode@gmail.com>
2024-11-18 15:44:38 +0530

    first commit

---
+++ script/languages.js
@@ -0,0 +1,13 @@
+const languages = [
+  {
+    no: "0",
+    name: "Auto",
+    native: "Detect",
+    code: "auto",
+  },
+  {
+    no: "27",
+    name: "Hindi",
+    native: "हिन्दी",
+    code: "hi",
+  },];
